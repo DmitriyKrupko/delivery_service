2e9281b625ec2f6daaf29da623c967d8e06089d7
DmitriyKrupko <dimonkrupko@yandex.ru>
2025-02-06 20:55:38 +0300

    add_html_page

--- delivery/delivery_service/restaurants/views.py
+++ delivery/delivery_service/restaurants/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from .models import Restaurant
+from django.shortcuts import render, get_object_or_404
+from .models import Restaurant, Dish
 
 # Create your views here.
 def index(request):
     restaurants = Restaurant.objects.all()
     return render(request, 'restaurants/index.html', {'restaurants': restaurants})
+
+def restaurant_dishes(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    dishes = restaurant.dishes.all()  # Получаем все блюда, связанные с рестораном
+
+    context = {
+        'restaurant': restaurant,
+        'dishes': dishes,
+    }
+    return render(request, 'restaurants/restaurant_dishes.html', context)
